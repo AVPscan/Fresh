7b50c3b3a6aa196d444d84e991d82ebaaabd4e5e
AVPscan <avp70ru@mail.ru>
2026-05-16 22:12:50 +0700

    вернулся к 8 цветам, попробую создать генерацию для 16 цветов

--- engine.c
+++ engine.c
@@ -109,34 +109,28 @@ ugoc Keys(void) { ugoc s = Off; uint8_t d, c = Buf.push;
   while(c != Buf.pop) { d = *AKey(c--); if (d & b7) s++; if (d & b6) s++; } return s; }
 
 void Print(uint8_t n, char *str) { if (!n || !str) return;
-  char *dst = Cdbuf; ugoc len = StrLen(str); n &= ~b7; PalBuf* pal = APal(n); MemCpy(dst, pal->d, pal->l); dst += pal->l;
+  char *dst = Cdbuf; ugoc len = StrLen(str); n &= ~b76; PalBuf* pal = APal(n); MemCpy(dst, pal->d, pal->l); dst += pal->l;
   MemCpy(dst, str, len); SysWrite(Cdbuf, dst + len - Cdbuf); }
 void BPrint(uint8_t n, char *str) { if (!str) return;
-  char *dst = Cdbuf; ugoc len = StrLen(str); n &= b3210; PalBuf* pal = AFon(n); MemCpy(dst, pal->d, pal->l); dst += pal->l;
+  char *dst = Cdbuf; ugoc len = StrLen(str); n &= b210; PalBuf* pal = AFon(n); MemCpy(dst, pal->d, pal->l); dst += pal->l;
   MemCpy(dst, str, len); if (n != Convas.Fone) { dst += len; pal = AFon(Convas.Fone); len = pal->l; MemCpy(dst, pal->d, len); }
   SysWrite(Cdbuf, dst + len - Cdbuf); }
 void InitVram(Cell addr, Cell size) { if (!addr || (size < SizeVram)) return;
-  PalBuf *pal, *mode, *src; uint32_t in, t;
-  uint8_t i, j, ct, c, k = On, p = b3, z = 16, y = z - On, x = 2, h[] = { 0, 1, 8, 14, 2, 4, 9, 6, 3, 8, 11, 10, 5, 12, 13, 15 };
-  char* base = (char*)addr; char* cbase[] = { "\3\33[3", "\6\33[38;5", "\6\33[38;2" }; 
+  Cdata = (char*)addr; Cinfo = (uint8_t*)(Cdata + SDCell); Cds = Cinfo + SInfo; Coffset = (ugoc*)(Cds + SDs); Cdfon = (char*)(Coffset + SOffset);
+  Cdpal = Cdfon + SFon; Cdkey = (uint8_t*)(Cdpal + SPal); Cdcon = (ugoc*)(Cdkey + SKeys); Cdwin = Cdcon + SConvas; Cvsw = Cdwin + SWin;
+  Ccsw = Cvsw + SVsw; Cevent = (char*)(Ccsw + SCsw); Cexec = Cevent + SEvent; Cdbuf = Cexec + SExec; VP.Win = MAX_WIN; Convas.Win = VP.Win; 
+  Convas.Min = Off; Convas.Max = VP.Win; Convas.D = Off; Convas.S = VP.Win; Convas.CW = CellLine; Convas.W = Convas.CW; Convas.CH = CellStr;
+  Convas.H = Convas.CH; Convas.Fone = FFone; Convas.Border = FBorder; VP.Mode = b2; VP.Loop = On; Vector(K_Mouse) = RPEncode;
+  PalBuf *pal, *mode, *src; char *base, *cbase[] = { "\3\33[3", "\6\33[38;5", "\6\33[38;2" }; uint32_t in, t, p = 256;
   char* modes[] = { "\12;22;23;27m", "\11;22;23;7m", "\11;23;27;1m", "\10;23;1;7m", "\11;22;27;3m", "\10;22;3;7m", "\10;27;1;3m", "\7;1;3;7m" };
-  Cdata = base; Cinfo = (uint8_t*)(base + SDCell); Cds = Cinfo + SInfo; Coffset = (ugoc*)(Cds + SDs); Cdfon = (char*)(Coffset + SOffset); Cdpal = Cdfon + SFon;
-  Cdkey = (uint8_t*)(Cdpal + SPal); Cdcon = (ugoc*)(Cdkey + SKeys); Cdwin = Cdcon + SConvas; Cvsw = Cdwin + SWin; Ccsw = Cvsw + SVsw;
-  Cevent = (char*)(Ccsw + SCsw); Cexec = Cevent + SEvent; Cdbuf = Cexec + SExec; if (CFDeep < b3) { k--; p = 3; } else if (CFDeep > b3) { k++; p = 24; }
-  while(x--) { c = z;
-    while(c--) { src = AFon(c); mode = (PalBuf*)cbase[k];
-      ct = (x) ? h[c] : c; in = (ct) ? (uint32_t)((((1 << p) * ct) / y) - On) : Off; if (!k && ct && ct != y) in++;
-      src->l = mode->l; MemCpy(src->d, mode->d, src->l); base = (char*)(src->d + src->l); i = src->l; j = On;
+  uint8_t i, j, ct, c, k = On, x = 2, h[] = { 0, 4, 1, 5, 2, 6, 3, 7 }; if (CFDeep < b3) { k--; p = b3; } else if (CFDeep > b3) { k++; p = 1 << 24; }
+  while(x--) { c = b3; while(c--) { ct = (x) ? h[c] : c; in = (ct) ? (((p * ct) / 7) - On) : Off; if (!k && ct && ct != 7) in++;
+      src = AFon(c); mode = (PalBuf*)cbase[k]; j = On; src->l = mode->l; MemCpy(src->d, mode->d, src->l); base = (char*)(src->d + src->l); i = src->l;
       if (k > On) { j += b1; } while(j--) { t = in & 255; in >>= b3; if (k) { *base++ = ';'; i++; }
         if (t / 100) { *base++ = 0x30 + (t / 100); t %= 100; i++; } if (t / 10) { *base++ = 0x30 + (t / 10); t %= 10; i++; } *base++ = 0x30 + t; i++; }
-      *base = 'm'; src->l = i + On; } c = z;
-    while(c--) { src = AFon(c); i = b3;
-      while(i--) { pal = APal((i << b2) + c); mode = (PalBuf*)modes[i]; pal->l = src->l + mode->l - On;
-        MemCpy(pal->d, src->d, src->l - On); MemCpy(pal->d + src->l - On, mode->d, mode->l); }
-      *(src->d + b1) = '4'; } SwitchPal(); }
-  VP.Win = MAX_WIN; Convas.Win = VP.Win; Convas.Min = Off; Convas.Max = VP.Win; Convas.D = Off; Convas.S = VP.Win;
-  Convas.CW = CellLine; Convas.W = Convas.CW; Convas.CH = CellStr; Convas.H = Convas.CH; Convas.Fone = FFone; Convas.Border = FBorder;
-  VP.Mode = b2; VP.Loop = On; Vector(K_Mouse) = RPEncode; }
+      *base = 'm'; src->l = i + On; i = b3; while(i--) { pal = APal((i << b10) + c); mode = (PalBuf*)modes[i]; pal->l = src->l + mode->l - On;
+        MemCpy(pal->d, src->d, src->l - On); MemCpy(pal->d + src->l - On, mode->d, mode->l); *(pal->d + src->l - On + mode->l) = Off; }
+      *(src->d + b1) = '4'; } SwitchPal(); } }
 Cell SystemSwitch(void) {
   if (VRam.SystemSwitch) { VRam.size = SizeVram; if (!(VRam.addr = GetRam(&VRam.size))) return Off;
     VRam.SystemSwitch--; SWD(VRam.addr); InitVram(VRam.addr,VRam.size); SwitchRaw(); Delay_ms(Off); IRnd();
@@ -206,8 +200,8 @@ void Anchor(void) { if (VP.Mode ^= b1) { } else { Convas.W = Convas.CW; Convas.H
 void Bye(void) { VP.Loop = Off; }
 void SwitchPal(void) { char* a = (char*)(Coffset + SOffset); Cdpal = (char*)(a + SFon);
   if (a == Cdfon) { a += SFon / 2; Cdpal += SPal / 2; } Cdfon = a; }
-void WSwitch(void) { if (Win(VP.Wec)->Xr) Win(VP.Wec)->WF ^= b7; }
-void WASwitch(void) { if (Win(VP.Wec)->WF ^= b7) WinView(VP.Wec); }
+void WSwitch(void) { if (Win(VP.Wec)->Xr) Win(VP.Wec)->EF ^= b1; }
+void WASwitch(void) { if (Win(VP.Wec)->EF ^= b1) WinView(VP.Wec); }
 void WinDown(void) { if (Convas.D) { uint16_t l = Convas.D; Win(--l)->Layer = Off; while(l) ++Win(--l)->Layer; } }
 void WinUp(void) { if (Convas.D) { uint16_t l = Convas.D; Win(Off)->Layer = --l; while(l) --Win(--l)->Layer; } }
 
--- sys.h
+++ sys.h
@@ -27,7 +27,7 @@
 #define CellPow   13                          // Масштаб холста 13 16к, 14 32к, 15 64к.... 
 #define MAX_WIN   512                         // Максимально число окон на холсте
 #define MaxSpeed  (1 << (CellPow - 4))        // Максимальное ускорение курсора
-#define CFDeep    3                          // Глубина {3 8 24} бита цветности
+#define CFDeep    24                          // Глубина {3 8 24} бита
 #define CFH       1                           // Вы гуманитарий?) На самом деле сложнее, нужна ли сортировка яркости {виновна в этом IBM},
                                               // можно не менять создаётся две палитры {в альтернативной сортировки нет}
 #if CellPow < 15                              // Создаём новый тип данных, достаточный для работы с нужным разрешением, 
@@ -66,23 +66,24 @@ enum {
     K_DOW, K_HOM, K_END, K_PUP, K_PDN, K_INS, K_F1, K_F2,
     K_F3, K_F4, K_F5, K_F6, K_F7, K_F8, K_F9, K_F10,
     K_F11, K_F12, K_F13, K_F14, K_F15, K_ALT_TAB, K_ALT_ENT, K_ALT_ESC, K_Mouse };
-enum { Fblack, f0, Fnavy, f1, Folive, f2, Fcyan, f3, Fmarsala, f4, Ffuchsia, f5, Fochre, f6, f7, Fwhite };
+enum { Fblack, Fnavy, Folive, Fcyan, Fmarsala, Ffuchsia, Fochre, Fwhite };
 enum {
-    black, c0, navy, c1, olive, c2, cyan, c3, marsala, c4, fuchsia, c5, ochre, c6, c7, white,
-    blackI, cI0, navyI, cI1, oliveI, cI2, cyanI, cI3, marsalaI, cI4, fuchsiaI, cI5, ochreI, cI6, cI7, whiteI,
-    blackB, cB0, navyB, cB1, oliveB, cB2, cyanB, cB3, marsalaB, cB4, fuchsiaB, cB5, ochreB, cB6, cB7, whiteB,
-    blackBI, cBI0, navyBI, cBI1, oliveBI, cBI2, cyanBI, cBI3, marsalaBI, cBI4, fuchsiaBI, cBI5, ochreBI, cBI6, cBI7, whiteBI,
-    blackC, cC0, navyC, cC1, oliveC, cC2, cyanC, cC3, marsalaC, cC4, fuchsiaC, cC5, ochreC, cC6, cC7, whiteC,
-    blackCI, cCI0, navyCI, cCI1, oliveCI, cCI2, cyanCI, cCI3, marsalaCI, cCI4, fuchsiaCI, cCI5, ochreCI, cCI6, cCI7, whiteCI,
-    blackCB, cCB0, navyCB, cCB1, oliveCB, cCB2, cyanCB, cCB3, marsalaCB, cCB4, fuchsiaCB, cCB5, ochreCB, cCB6, cCB7, whiteCB,
-    blackCBI, cCBI0, navyCBI, cCBI1, oliveCBI, cCBI2, cyanCBI, cCBI3, marsalaCBI, cCBI4, fuchsiaCBI, cCBI5, ochreCBI, cCBI6, cCBI7, whiteCBI };
+    black, navy, olive, cyan, marsala, fuchsia, ochre, white,
+    blackI, navyI, oliveI, cyanI, marsalaI, fuchsiaI, ochreI, whiteI,
+    blackB, navyB, oliveB, cyanB, marsalaB, fuchsiaB, ochreB, whiteB,
+    blackBI, navyBI, oliveBI, cyanBI, marsalaBI, fuchsiaBI, ochreBI, whiteBI,
+    blackC, navyC, oliveC, cyanC, marsalaC, fuchsiaC, ochreC, whiteC,
+    blackCI, navyCI, oliveCI, cyanCI, marsalaCI, fuchsiaCI, ochreCI, whiteCI,
+    blackCB, navyCB, oliveCB, cyanCB, marsalaCB, fuchsiaCB, ochreCB, whiteCB,
+    blackCBI, navyCBI, oliveCBI, cyanCBI, marsalaCBI, fuchsiaCBI, ochreCBI, whiteCBI };
 #define FFone   Fwhite
 #define FBorder Fblack
 typedef struct {
-    uint8_t color   : 4;                      // бит 3210   цвет
-    uint8_t inverse : 1;                      // бит 4      инверсия
-    uint8_t bold    : 1;                      // бит 5      толстый
-    uint8_t cursive : 1;                      // бит 6      курсив
+    uint8_t color   : 3;                      // бит 210    цвет
+    uint8_t inverse : 1;                      // бит 3      инверсия
+    uint8_t bold    : 1;                      // бит 4      толстый
+    uint8_t cursive : 1;                      // бит 5      курсив
+    uint8_t reserve : 1;                      // бит 6      
     uint8_t Refresh : 1;                      // бит 7      {1/0} есть изменения /нет изменений
 } Info;   
 typedef struct {                              //UTFinfo  
@@ -102,17 +103,17 @@ typedef struct { uint8_t l, d[31]; } PalBuf;
 typedef struct { uint8_t data1, tic1, data2, tic2, utf8[4]; } KeyBuf; 
 typedef struct { uint8_t C, N; uint16_t W; } Events;
 typedef struct {
-    uint8_t color   : 4;                      // бит 3210   цвет
-    uint8_t inverse : 1;                      // бит 4      инверсия
-    uint8_t bold    : 1;                      // бит 5      толстый
-    uint8_t cursive : 1;                      // бит 6      курсив
-    uint8_t vision  : 1;                      // бит 7      {1} отображается {0} не отображается
+    uint8_t color   : 4;                      // бит 210    цвет
+    uint8_t inverse : 1;                      // бит 3      инверсия
+    uint8_t bold    : 1;                      // бит 4      толстый
+    uint8_t cursive : 1;                      // бит 5      курсив
+    uint8_t cursor  : 1;                      // бит 6      {1} показывать {0} не показывать - курсор окна
+    uint8_t nowrap  : 1;                      // бит 7      {1} включен {0} выключен авто перенос строк окна
 } WF;
 typedef struct {
-    uint8_t cursor  : 1;                      // бит 0      {1} показывать {0} не показывать - курсор окна
-    uint8_t nowrap  : 1;                      // бит 1      {1} включен {0} выключен авто перенос строк окна
-    uint8_t sd      : 1;                      // бит 2      {1} статичное (не изменяется в размере на холсте, в байтах) {0} динамичное окно
-    uint8_t wait    : 1;                      // бит 3      {1} занято заливаются данные из файла/порта {0} свободно
+    uint8_t sd      : 1;                      // бит 0      {1} статичное (не изменяется в размере на холсте, в байтах) {0} динамичное окно
+    uint8_t vision  : 1;                      // бит 1      {1} отображается {0} не отображается
+    uint8_t wait    : 1;                      // бит 2      {1} занято заливаются данные из файла/порта {0} свободно
 } EF;
 typedef struct { ugoc W, H, CW, CH; uint16_t Win, Min, Max, D, S; uint8_t Fone, Border; } Canalysis;
 typedef struct { goc Xr, Yr; ugoc W, H, MaxCs, MaxVs, MaxH, XCur, YCur, WFirstSR, Xc, Yc; uint16_t Layer, parent, child; uint8_t WF, EF; } Windows;
@@ -125,8 +126,8 @@ enum {
     SInfo = ConvasArea,                                                       // Размер атрибутов для ячеек холста (Info)
     SDs = ConvasArea,                                                         // Размер информации о ячейках холста (Data/Structure)
     SOffset = ConvasArea * sizeof(ugoc) / 2,                                  // Размер смещений для ячеек холста
-    SFon = 2 * 16 * sizeof(PalBuf),                                           // Размер 2х буферов фона (16 цветов)
-    SPal = 2 * 8 * 16 * sizeof(PalBuf),                                       // Размер 2х буферов палитр (8 режимов по 16 цветов)
+    SFon = 2 * 8 * sizeof(PalBuf),                                            // Размер 2х буферов фона (8 цветов)
+    SPal = 2 * 8 * 8 * sizeof(PalBuf),                                        // Размер 2х буферов палитр (8 режимов по 8 цветов)
     SKeys = SKey * sizeof(KeyBuf),                                            // Размер данных кольцевого буфера клавиатуры
     SConvas = sizeof(Canalysis) / 2,                                          // Размер данных под разбивку холста для организации окон
     SWin = MAX_WIN * sizeof(Windows) / 2,                                     // Размер данных для окон
